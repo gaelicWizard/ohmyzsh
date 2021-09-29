88fab264f0f9a1ebb46cca1c91d3c846a40f9e9f
John D Pell <John+git@gaelicWizard.net>
2021-09-28 23:05:30 -0500

    history: use `[[`
    Just a stickler for consistency

--- lib/history.zsh
+++ lib/history.zsh
@@ -27,9 +27,9 @@ case ${HIST_STAMPS-} in
 esac
 
 ## History file configuration
-[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
-[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+[[ -z "$HISTFILE" ]] && HISTFILE="$HOME/.zsh_history"
+[[ "$HISTSIZE" -lt 50000 ]] && HISTSIZE=50000
+[[ "$SAVEHIST" -lt 10000 ]] && SAVEHIST=10000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
